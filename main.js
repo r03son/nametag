5684222562de1fa3ab75f870d080577fd1969d61
Robson Silva <92696337+r03son@users.noreply.github.com>
2021-10-31 21:03:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+const LinksSocialMedia = {
+  github: 'r03son',
+  youtube: 'channel/UC59eiGbQFmxuauAgPQ6KKEQ',
+  instagram: 'robson.s.insta',
+  twitter: 'robter'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+}
+// i = i + 1
+// i++
+
+// ser humano 12345678910
+//computador 0123456789
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.texContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
